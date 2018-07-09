ebecbfa12ae2245a34e4f02031c86bc5659a392b
Rajasekhar Ghanta <rajasekhar.ghanta@gmail.com>
2018-07-09 18:16:15 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+require('./config/config');
+
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '..', '/public');
+const port = process.env.PORT;
+const app = express();
+
+app.use(express.static(publicPath));
+
+
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`)
+});
