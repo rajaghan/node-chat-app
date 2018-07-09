6820fa8816269bc6135b32967fdfa296ba22a72c
Rajasekhar Ghanta <rajasekhar.ghanta@gmail.com>
2018-07-09 21:11:35 +0530

    broadcast

--- server/server.js
+++ server/server.js
@@ -16,10 +16,24 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   //listen to incoming event 'createMessage'
   socket.on('createMessage', (newMsg) => {
     //emit an event and pass data to all connected users
     io.emit('newMessage', {
+      //emit an event and pass data to all connected users except the emitter
+      // socket.broadcast.emit('newMessage', {
       from: newMsg.from,
       text: newMsg.text,
       createdAt: new Date().getTime()
